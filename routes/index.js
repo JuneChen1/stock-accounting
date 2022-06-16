0685f1b756effcbb295c500151e2f782107514a3
june850713@gmail.com <june850713@gmail.com>
2022-06-16 20:12:50 +0800

    feat: add the new page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
+const stocks = require('./modules/stocks')
 
 router.use('/', home)
+router.use('/stocks', stocks)
 
 module.exports = router
