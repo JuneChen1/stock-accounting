a45833abed313ae4461edb8e34496d8fa6717770
june850713@gmail.com <june850713@gmail.com>
2022-06-16 20:18:40 +0800

    feat: add record schema

---
+++ models/stock.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema()
+const recordSchema = new Schema({
+  symbol: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  method: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: Number
+  },
+  shares: {
+    type: Number
+  },
+  date: {
+    type: Date,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Record', recordSchema)
