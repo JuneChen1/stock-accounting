2ce4ed6b2e804be7e1d70749ea6f28f5a73d7015
june850713@gmail.com <june850713@gmail.com>
2022-06-25 12:14:30 +0800

    refactor: modify Promise function in home route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
       stocks.forEach(stock => {
         symbolString += `tse_${stock.symbol}.tw|`
       })
-      const renderStoks = new Promise((resolve, reject) => {
+      new Promise((resolve, reject) => {
         const BASE_URL = 'https://mis.twse.com.tw/stock/api/getStockInfo.jsp?json=1&delay=0&ex_ch='
         axios.get(BASE_URL + symbolString)
           .then(function (response) {
@@ -26,23 +26,23 @@ router.get('/', (req, res) => {
             for (let i = 0; i < dataArray.length; i++) {
               stocks[i].price = Math.floor(dataArray[i].z * 100) / 100
             }
-            resolve('success')
-            reject(Error)
+            resolve()
           }).catch(function (error) {
+            reject(Error)
             console.log(error)
           })
       })
-      renderStoks.then(() => {
-        stocks.forEach(stock => {
-          // calculate average cost
-          const cost = Math.round((stock.value / stock.shares) * 10) / 10
-          stock.cost = cost
-          // calculate profit and loss
-          const profit = Math.round(((stock.price - cost) / cost) * 100) + '%'
-          stock.profit = profit
+        .then(() => {
+          stocks.forEach(stock => {
+            // calculate average cost
+            const cost = Math.round((stock.value / stock.shares) * 10) / 10
+            stock.cost = cost
+            // calculate profit and loss
+            const profit = Math.round(((stock.price - cost) / cost) * 100) + '%'
+            stock.profit = profit
+          })
+          res.render('index', { stocks })
         })
-        res.render('index', { stocks })
-      })
     })
 })
 
