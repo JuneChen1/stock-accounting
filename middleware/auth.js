a6f9386985a30b7eaf341b08c65d3be278a26be8
june850713@gmail.com <june850713@gmail.com>
2022-08-13 17:54:18 +0800

    feat: add api authenticated

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,5 +5,11 @@ module.exports = {
     }
     req.flash('error_msg', '請先登入才能使用')
     res.redirect('/users/login')
+  },
+  apiAuthenticated: (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.status(401).json({ status: 'Unauthorized', message: '請先登入才能使用' })
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ const home = require('./modules/home')
 const stocks = require('./modules/stocks')
 const api = require('./modules/api')
 const users = require('./modules/users')
-const { authenticated } = require('../middleware/auth')
+const { authenticated, apiAuthenticated } = require('../middleware/auth')
 
 router.use('/users', users)
 router.use('/stocks', authenticated, stocks)
-router.use('/api', authenticated, api)
+router.use('/api', apiAuthenticated, api)
 router.use('/', authenticated, home)
 
 module.exports = router
